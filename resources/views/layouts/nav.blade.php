da901d4ddf27b67ddb540de41ede8b7cad633380
unknown <jivnath@gmail.com>
2018-08-10 22:46:24 +0545

    menu set after login

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,12 +1,11 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-    <div class="container">
+    <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'PSI') }}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto" style="margin-left: 75px;">
