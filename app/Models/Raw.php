33803c3e208c1b526c2ded95ce944af9628226f2
Nivesh Acharya <prexevin@gmail.com>
2018-08-23 20:56:36 +0545

    created total necessary controller and others

---
+++ app/Models/Raw.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class Raw extends Model
+{
+	public static function totalNecessary()
+	{
+		DB::select("SELECT
+    ctt.company_id,
+    (
+        SELECT
+            name
+        FROM
+            companies c
+        WHERE
+            c.id = ctt.company_id
+    ) company_name,
+    companytt_id time_table_id,
+    cts.id AS rel_id,
+   
+    DATE,
+    time,
+    (help+normal) necessary,
+     (
+        SELECT
+            COUNT(*) occupied_shift
+        FROM
+            company_to_employees_rel cte
+        WHERE
+            cte.cts_id = cts.id
+    ) total_occupied_shift
+FROM
+    company_time_schedules cts,
+    company_time_tables ctt
+WHERE
+    cts.companytt_id = ctt.id
+    AND companytt_id = 12;");
+	}
+}
