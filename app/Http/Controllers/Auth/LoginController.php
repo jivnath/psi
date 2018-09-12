5c533348de4d326335362097e932771f21a1f4d7
Pramod Katuwal <pramodktwl@gmail.com>
2018-09-12 16:39:00 +0545

    fix auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -50,7 +50,6 @@ class LoginController extends Controller
 
     public function logout()
     {
-        $user = Auth::user();
         Auth::logout();
         Session::flush();
         return redirect(property_exists($this, 'redirectAfterLogout') ? $this->redirectAfterLogout : '/');
