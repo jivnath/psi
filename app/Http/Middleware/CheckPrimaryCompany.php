9898bc195ba844ecadc6814ee05833e23b378cf5
Pramod Katuwal <pramodktwl@gmail.com>
2018-09-13 19:07:40 +0545

    fix redirect and get company detail

--- app/Http/Middleware/CheckPrimaryCompany.php
+++ app/Http/Middleware/CheckPrimaryCompany.php
@@ -1,23 +1,25 @@
 <?php
-
 namespace App\Http\Middleware;
-use Illuminate\Support\Facades\Auth;
 
+use Illuminate\Support\Facades\Auth;
 use Closure;
 
 class checkPrimaryCompany
 {
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if(Auth::user()->primary_company != null)
+        if (\Session::get('primary_company')->name == null)
 
-        return $next($request);
+            return $next($request);
+        else
+            return redirect('/home');
     }
 }
