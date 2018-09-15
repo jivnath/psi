641ab69b312b530f0d9fad0e171011a815401fda
a-nivesh <acharyanivesh@gmail.com>
2018-09-14 17:47:19 -0700

    fixed user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,51 +2,54 @@
 
 @section('content')
 
-<div class='col-md-4 offset-4'>
+<div class='col-md-8 offset-2'>
 
-    <h1><i class='fa fa-user-plus'></i> Edit {{$user->name}}</h1>
+    <h4><i class='fa fa-user-plus'></i> Edit {{$user->name}}</h4>
     <hr>
 
     {{ Form::model($user, array('route' => array('users.update', $user->id), 'method' => 'PUT')) }}{{-- Form model binding to automatically populate our fields with user data --}}
 
     <div class="form-group">
-        {{ Form::label('name', 'Name:') }}
+       <b> {{ Form::label('name', 'Name:') }}</b>
         {{ $user->name }}
     </div>
 
     <div class="form-group">
-        {{ Form::label('email', 'Email:') }}
+        <b>{{ Form::label('email', 'Email:') }}</b>
         {{ $user->email }}
     </div>
 
-    <h5><b>Give Role</b></h5>
+    <b>Give Role</b>
 
     <div class='form-group'>
-        <select name="role">
+        <select name="role" class="form-control" style="width: 60%">
             @foreach($roles as $role)
                 <option value="{{$role['name']}}" <?=($user->roles()->pluck('name')->first()==$role['name'])? 'selected="selected"':''?>> {{$role['name']}} </option>
             @endforeach
         </select>
+    </div>
         <div class="form-group">
-            <label for="companies">Companies</label><br>{{dd($comp_id)}}
+            <b><label for="companies">Companies</label></b><br>
 
             @foreach($allCompanies->chunk(3) as $chunk)
+                <?php
+                ?>
                 <div class="row">
                     @foreach($chunk as $comp)
                         <div class="col-md-4">
-                            <input type="checkbox" name="comapnies[]" value="{{$comp->id}}"<?=(in_array($comp->id, $comp_id))?'checked':''?>>
+                            <input type="checkbox" name="companies[]" value="{{$comp->id}}"<?=(in_array($comp->id, $compid))?'checked':''?>>
                             {{Form::label($comp->name, $comp->name)}}
                         </div>
                     @endforeach
                 </div>
             @endforeach
         </div>
-    </div>
 
     {{ Form::submit('Edit', array('class' => 'btn btn-primary')) }}
 
     {{ Form::close() }}
 
 </div>
+
 
 @endsection
