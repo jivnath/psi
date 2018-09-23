e411618ce4ae091a998b18068f26dd4e9b3978db
Pramod Katuwal <pramodktwl@gmail.com>
2018-09-23 20:45:20 +0545

    fix table

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -1,64 +1,61 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('content')
 
-@section('content')
+<section class="content">
+	<div class="row">
+		<div id="validation" class="alert alert-danger" style="display: none"></div>
+		<div class="col-md-12">
+			<div class="box box-info">
+				<div class="box-header with-border">
+					<h3 class="box-title">Employees List</h3>
+				</div>
+				<div class="box-body">
+				<div>
+					<table class="table table-striped table-fixed"
+						style="text-align: center">
+						<thead>
+							<tr>
+								@foreach($columns as $column)
+								<th class="sticky" style="word-wrap: break-word">{{__('employee.'.
+									ucwords(str_replace('_','',ucwords($column,'_'))))}}</th>
+								@endforeach
 
+							</tr>
+						</thead>
+						<tbody>
+							@if(count($cells) > 0) @foreach($cells as $index => $cell)
+							<tr>
+								@foreach($columns as $column) @if($column != 'updated_at' &&
+								$column != 'psi_number')
+								<td class="contenteditable" data-column="{{ $column }}"
+									data-old="" contenteditable="true">@if ($column == 'sex') <select
+									name="sex"> @foreach($sex as $s)
+										<option
+											<?= ($cell->$column==$s->name)? 'selected="selected"':''?>
+											value="$s->id">{{ $s->name }}</option> @endforeach
+								</select> @else {{ $cell->{$column} }} @endif
+								</td> @else
+								<td>{{ $cell->{$column} }}</td> @endif @endforeach
+							</tr>
+							@endforeach @else
+							<tr>
+								<td colspan="6">No sheets are uploaded</td>
+							</tr>
+							@endif
+						</tbody>
+					</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
-    <div id="validation" class="alert alert-danger" style="display:none"></div>
-    <div class="container">
-        <div style="background-color: rgb(255,255,255);">
-         {{--   <h1>{{ $companyToEmployee->first()->company->name }}</h1>--}}<h1>Employees</h1>
-        </div>
-        <table class="table table-striped table-fixed" style="text-align: center" >
-             <thead>
-                <tr>
-                    @foreach($columns as $column)
-                        <th class="sticky" style="word-wrap:break-word">{{__('employee.'. ucwords(str_replace('_','',ucwords($column,'_'))))}}</th>
-                    @endforeach
-
-                </tr>
-            </thead>
-            <tbody>
-                @if(count($cells) > 0)
-                    @foreach($cells as $index => $cell)
-                        <tr>
-                            @foreach($columns as $column)
-                                @if($column != 'updated_at' && $column != 'psi_number')
-                                    <td class="contenteditable"
-                                        data-column = "{{ $column }}"
-                                        data-old = "" 
-                                        contenteditable="true">
-                                        @if ($column == 'sex')
-                                            <select name="sex">
-                                            @foreach($sex as $s)
-                                                <option <?= ($cell->$column==$s->name)? 'selected="selected"':''?> value="$s->id"> {{ $s->name }} </option>
-                                            @endforeach                                                
-                                            </select>
-                                        @else
-                                            {{ $cell->{$column} }}
-                                        @endif
-                                    </td>
-                                @else
-                                    <td>{{ $cell->{$column} }}</td>
-                                @endif
-                            @endforeach
-                        </tr>
-                    @endforeach
-                @else 
-                    <tr>
-                        <td colspan="6">No sheets are uploaded</td>
-                    </tr>
-                @endif
-            </tbody>
-        </table>
-    </div>
-    {{--<input type="hidden" id="companyId" value ="{{ $companyId }}"/>--}}
-@endsection
-
-
-@push('scripts')
+@endsection @push('scripts')
 
 <script>
     $(document).ready(function(){
+        $('.box-body div').css({'width':window.innerWidth-90,'overflow': 'scroll','height':window.innerHeight/1.5});
+
         var tds = document.querySelectorAll("td.contenteditable");
         tds.forEach(function(el, index){
             employee.inlineEditable(el, function(response){
