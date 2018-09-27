2f310a83d27b99a85151cbd798551a9499d9d488
a-nivesh <acharyanivesh@gmail.com>
2018-09-27 15:19:04 -0700

    changes on viber alert setting

--- resources/views/viber/alert_setting.blade.php
+++ resources/views/viber/alert_setting.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 @section('content')
+    @php
+        $intervalType=['year', 'month', 'day', 'hrs', 'min', 'sec'];
+    @endphp
     <div class="container-fluid">
-        <div class="card">
-            <div class="card-body">
-                <h3 style="text-align: center">Viber Alert Setting</h3>
-                <br>
-                <br>
-                <div class="row">
+        <div class="box box-info">
+            <div class="box-header"><h3 style="text-align: center">Viber Alert Setting</h3></div>
+            <div class="box-body">
+                <div class="row" style="margin-top: 30px">
                     <div class="col-md-8 offset-2">
                         <div name="employee_master_data">
                             <h5 style="text-decoration: underline;">Employee Master Data</h5>
@@ -15,15 +16,26 @@
                                     <label for="card_expiry">Residance Card Expiry </label>
                                 </div>
                                 <div class="col-md-8">
-                                    <table width="60%">
+                                    <table width="60%" style="text-align: center">
                                         <col style="width: 20%">
                                         <col style="width: 20%">
                                         <col style="width: 20%">
                                         <tbody>
                                         <tr>
-                                            <td style="border: 1px solid black">30 </td>
-                                            <td style="border: 1px solid black">Day<i class="fa fa-sort-down"></i></td>
-                                            <td><i><small>before</small></i></td>
+                                            <td id="residanceInt" class="contenteditable" data-old=""
+                                                contenteditable="true"
+                                                style="border: 1px solid black">30
+                                            </td>
+                                            <td class="contenteditable" data-old="" contenteditable="true"
+                                                style="border: 1px solid black">
+                                                <select id="residanceType" style="border: 0px; outline:0px">
+                                                    @foreach($intervalType as $type)
+                                                        <option value="{{$type}}">{{$type}}</option>
+                                                    @endforeach
+                                                </select></td>
+                                            <td><i>
+                                                    <small>before</small>
+                                                </i></td>
 
                                         </tr>
                                         </tbody>
@@ -41,19 +53,36 @@
                                     <label for="card_expiry">Before 1 Day </label>
                                 </div>
                                 <div class="col-md-8">
-                                    <table width="65%" style="text-align: center">
+                                    <table width="80%" style="text-align: center">
                                         <col style="width: 15%">
                                         <col style="width: 15%">
                                         <col style="width: 15%">
-                                        <col style="width: 5%">
+                                        <col style="width: 10%">
                                         <col style="width: 15%">
                                         <tbody>
                                         <tr>
-                                            <td style="border: 1px solid black">15 </td>
-                                            <td style="border: 1px solid black">Min &nbsp <i class="fa fa-sort-down"></i></td>
-                                            <td><i><small>interval</small></i></td>
-                                            <td style="border: 1px solid black">5</td>
-                                            <td><i><small>Total Count</small></i></td>
+                                            <td id="beforeOneDayInt" class="contenteditable" data-old=""
+                                                contenteditable="true"
+                                                style="border: 1px solid black">15
+                                            </td>
+                                            <td class="contenteditable" data-old="" contenteditable="true"
+                                                style="border: 1px solid black">
+                                                <select id="beforeOneDayType" style="border: 0px; outline:0px">
+                                                    @foreach($intervalType as $type)
+                                                        <option value="{{$type}}">{{$type}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </td>
+                                            <td><i>
+                                                    <small>interval</small>
+                                                </i></td>
+                                            <td id="beforeOneDayCount" class="contenteditable" data-old=""
+                                                contenteditable="true"
+                                                style="border: 1px solid black">5
+                                            </td>
+                                            <td><i>
+                                                    <small>Total Count</small>
+                                                </i></td>
 
                                         </tr>
                                         </tbody>
@@ -66,19 +95,35 @@
                                     <label for="card_expiry">3 Hours Before </label>
                                 </div>
                                 <div class="col-md-8">
-                                    <table width="65%" style="text-align: center">
+                                    <table width="80%" style="text-align: center">
                                         <col style="width: 15%">
                                         <col style="width: 15%">
                                         <col style="width: 15%">
-                                        <col style="width: 5%">
+                                        <col style="width: 10%">
                                         <col style="width: 15%">
                                         <tbody>
                                         <tr>
-                                            <td style="border: 1px solid black">15 </td>
-                                            <td style="border: 1px solid black">Min &nbsp <i class="fa fa-sort-down"></i></td>
-                                            <td><i><small>interval</small></i></td>
-                                            <td style="border: 1px solid black">5</td>
-                                            <td><i><small>Total Count</small></i></td>
+                                            <td id="beforeThreeInt" class="contenteditable" data-old=""
+                                                contenteditable="true"
+                                                style="border: 1px solid black">15
+                                            </td>
+                                            <td class="contenteditable" data-old="" contenteditable="true"
+                                                style="border: 1px solid black">
+                                                <select id="beforeThreeType" style="border: 0px; outline:0px">
+                                                    @foreach($intervalType as $type)
+                                                        <option value="{{$type}}">{{$type}}</option>
+                                                    @endforeach
+                                                </select></td>
+                                            <td><i>
+                                                    <small>interval</small>
+                                                </i></td>
+                                            <td id="beforeThreeCount" class="contenteditable" data-old=""
+                                                contenteditable="true"
+                                                style="border: 1px solid black">5
+                                            </td>
+                                            <td><i>
+                                                    <small>Total Count</small>
+                                                </i></td>
 
                                         </tr>
                                         </tbody>
@@ -91,24 +136,45 @@
                                     <label for="card_expiry">Arrival Conformation </label>
                                 </div>
                                 <div class="col-md-8">
-                                    <table width="65%" style="text-align: center">
+                                    <table width="80%" style="text-align: center">
                                         <col style="width: 15%">
                                         <col style="width: 15%">
                                         <col style="width: 15%">
-                                        <col style="width: 5%">
+                                        <col style="width: 10%">
                                         <col style="width: 15%">
                                         <tbody>
                                         <tr>
-                                            <td style="border: 1px solid black">15 </td>
-                                            <td style="border: 1px solid black">Min &nbsp <i class="fa fa-sort-down"></i></td>
-                                            <td><i><small>interval</small></i></td>
-                                            <td style="border: 1px solid black">5</td>
-                                            <td><i><small>Total Count</small></i></td>
+                                            <td id="arrivalInt" class="contenteditable" data-old=""
+                                                contenteditable="true"
+                                                style="border: 1px solid black">15
+                                            </td>
+                                            <td class="contenteditable" data-old="" contenteditable="true"
+                                                style="border: 1px solid black">
+                                                <select id="arrivalType" style="border: 0px; outline:0px">
+                                                    @foreach($intervalType as $type)
+                                                        <option value="{{$type}}">{{$type}}</option>
+                                                    @endforeach
+                                                </select></td>
+                                            <td><i>
+                                                    <small>interval</small>
+                                                </i></td>
+                                            <td id="arrivalType" class="contenteditable" data-old=""
+                                                contenteditable="true"
+                                                style="border: 1px solid black">5
+                                            </td>
+                                            <td><i>
+                                                    <small>Total Count</small>
+                                                </i></td>
 
                                         </tr>
                                         </tbody>
                                     </table>
                                 </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4 offset-4" style="text-align: right">
+                                <span style="margin-bottom:30px " id="submit" class="btn btn-primary">Save</span>
                             </div>
                         </div>
 
@@ -120,3 +186,21 @@
     </div>
 
 @endsection
+
+@push('scripts')
+    <script>
+        $('#submit').click(function(){
+           var expiryInt = $('#residanceInt').val();
+           var expiryType = $('#residanceType').val();
+           var beforeOneInt = $('#beforeOneDayInt').val();
+           var beforeOneType = $('#beforeOneDayType').val();
+           var beforeOneCount = $('#beforeOneDayCount').val();
+           var beforeThreeInt = $('#beforeThreeInt').val();
+           var beforeThreeType = $('#beforeThreeType').val();
+           var beforeThreeCount = $('#beforeOneDayCount').val();
+           var arrivalInt = $('#arrivalInt').val();
+           var arrivalType = $('#arrivalType').val();
+           var arrivalCount = $('#arrivalCount').val();
+        });
+    </script>
+@endpush
