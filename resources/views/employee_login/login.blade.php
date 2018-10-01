03490934ad2100effb92b3079ac061b1542796ab
a-nivesh <acharyanivesh@gmail.com>
2018-09-30 18:17:55 -0700

    Events on calendar

--- resources/views/employee_login/login.blade.php
+++ resources/views/employee_login/login.blade.php
@@ -12,9 +12,12 @@
                             @csrf
 
                             <div class="form-group row">
-                                <label for="email" class="col-sm-4 col-form-label text-md-right">@lang('dashboard.user')</label>
+                                <label for="email"
+                                       class="col-sm-4 col-form-label text-md-right">@lang('dashboard.user')</label>
                                 <div class="col-md-6">
-                                    <input id="psi_number" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="psi_number" value="{{ old('email') }}" required autofocus>
+                                    <input id="psi_number" type="text"
+                                           class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                           name="psi_number" value="{{ old('email') }}" required autofocus>
 
                                     @if ($errors->has('email'))
                                         <span class="invalid-feedback" role="alert">
@@ -25,15 +28,18 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">@lang('dashboard.pass')</label>
+                                <label for="password"
+                                       class="col-md-4 col-form-label text-md-right">@lang('dashboard.pass')</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                    <input id="password" type="password"
+                                           class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                           name="password" required>
 
                                     @if ($errors->has('password'))
                                         <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
@@ -41,7 +47,8 @@
                             <div class="form-group row">
                                 <div class="col-md-6 offset-md-4">
                                     <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        <input class="form-check-input" type="checkbox" name="remember"
+                                               id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                         <label class="form-check-label" for="remember">
                                             {{ __('login.remember_me') }}
--- resources/views/employees_dashboard/dashboard.blade.php
+++ resources/views/employees_dashboard/dashboard.blade.php
@@ -3,10 +3,15 @@
     <link rel="stylesheet" href="{{asset('bower_components/fullcalendar/dist/fullcalendar.min.css')}}">
     <link rel="stylesheet" href="{{asset('bower_components/fullcalendar/dist/fullcalendar.print.min.css')}}"
           media="print">
+    <style>
+        .fc-scroller {
+            overflow-x: visible !Important;
+        }
+    </style>
 
     <section class="content box box-success">
         <div class="col-md-6 offset-3">
-            <div class="form-group"  style="margin-top: 15px;">
+            <div class="form-group" style="margin-top: 15px;">
                 <label for="subsection"><h5>Sub-Section</h5></label>
                 <select class="form-control input-shorter" id="companies">
                     <option value="0">--Select sub section--</option>
@@ -54,52 +59,25 @@
     <div class="modal" id="ModalAdd" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <form class="form-horizontal" method="POST" action="#">
-
+                <form class="form-horizontal" method="POST" action="{{route('')}}">
+                    @csrf
                     <div class="modal-header">
-                        <h4 class="modal-title" id="myModalLabel">Add Schedule</h4>
+                        <h4 class="modal-title" id="myModalLabel"></h4>
                     </div>
                     <div class="modal-body">
 
                         <div class="form-group">
-                            <label for="title" class="col-sm-2 control-label">Title</label>
+                            <label for="color" class="col-sm-2 control-label">Shift</label>
                             <div class="col-sm-12">
-                                <input type="text" name="title" class="form-control" id="title" placeholder="Title">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="color" class="col-sm-2 control-label">Color</label>
-                            <div class="col-sm-12">
-                                <select name="color" class="form-control" id="color">
-                                    <option value="">Choose</option>
-                                    <option style="color:#0071c5;" value="#0071c5">&#9724; Dark blue</option>
-                                    <option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquoise</option>
-                                    <option style="color:#008000;" value="#008000">&#9724; Green</option>
-                                    <option style="color:#FFD700;" value="#FFD700">&#9724; Yellow</option>
-                                    <option style="color:#FF8C00;" value="#FF8C00">&#9724; Orange</option>
-                                    <option style="color:#FF0000;" value="#FF0000">&#9724; Red</option>
-                                    <option style="color:#000;" value="#000">&#9724; Black</option>
+                                <select name="color" class="form-control" id="shifts">
 
                                 </select>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="start" class="col-sm-5 control-label">Start date</label>
-                            <div class="col-sm-12">
-                                <input type="text" name="start" class="form-control" id="start" readonly>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="end" class="col-sm-5 control-label">End date</label>
-                            <div class="col-sm-12">
-                                <input type="text" name="end" class="form-control" id="end" readonly>
-                            </div>
-                        </div>
-
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Save changes</button>
+                        <button type="submit" class="btn btn-primary"> Apply </button>
                     </div>
                 </form>
             </div>
@@ -165,7 +143,7 @@
     <script src="{{asset('bower_components/fullcalendar/dist/fullcalendar.min.js')}}"></script>
     <script>
         var last_click = '';
-        $(document).ready(function(){
+        $(document).ready(function () {
             /* initialize the calendar
                   -----------------------------------------------------------------*/
             //Date for the calendar events (dummy data)
@@ -207,73 +185,90 @@
                     edit(event);
 
                 },
+                dayClick: function (date, allDay) {
+                    $('#myModalLabel').text(moment(date).format('YYYY-MM-DD'));
+                    var select = '<option value="0">--Choose Shift--</option>';
+                    $('#shifts').append(select);
+                    $('#calendar').fullCalendar('clientEvents', function (event) {
+
+                        if (moment(date).format('YYYY-MM-DD') == moment(event.start).format('YYYY-MM-DD')) {
+                            // do your stuff here
+                            $('#shifts').append('<option value="' + event.id + '">' + event.title + '</option>')
+                            console.log(event.id);
+                            console.log(event);
+
+
+                        }
+                    });
+                },
+
                 eventResize: function (event, dayDelta, minuteDelta, revertFunc) { // si changement de longueur
 
                     edit(event);
 
                 },
                 editable: true,
-                droppable: true, // this allows things to be dropped onto the calendar !!!
-                drop: function (date, allDay) { // this function is called when something is dropped
-
-                    // retrieve the dropped element's stored Event Object
-                    var originalEventObject = $(this).data('eventObject')
-
-                    // we need to copy it, so that multiple events don't have a reference to the same object
-                    var copiedEventObject = $.extend({}, originalEventObject)
-
-                    // assign it the date that was reported
-                    copiedEventObject.start = date
-                    copiedEventObject.allDay = allDay
-                    copiedEventObject.backgroundColor = $(this).css('background-color')
-                    copiedEventObject.borderColor = $(this).css('border-color')
-
-                    // render the event on the calendar
-                    // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-                    $('#calendar').fullCalendar('renderEvent', copiedEventObject, true)
-
-                    // is the "remove after drop" checkbox checked?
-                    if ($('#drop-remove').is(':checked')) {
-                        // if so, remove the element from the "Draggable Events" list
-                        $(this).remove()
-                    }
-
-                }
+                // droppable: true, // this allows things to be dropped onto the calendar !!!
+                // drop: function (date, allDay) { // this function is called when something is dropped
+                //
+                //     // retrieve the dropped element's stored Event Object
+                //     var originalEventObject = $(this).data('eventObject')
+                //
+                //     // we need to copy it, so that multiple events don't have a reference to the same object
+                //     var copiedEventObject = $.extend({}, originalEventObject)
+                //
+                //     // assign it the date that was reported
+                //     copiedEventObject.start = date
+                //     copiedEventObject.allDay = allDay
+                //     copiedEventObject.backgroundColor = $(this).css('background-color')
+                //     copiedEventObject.borderColor = $(this).css('border-color')
+                //
+                //     // render the event on the calendar
+                //     // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+                //     $('#calendar').fullCalendar('renderEvent', copiedEventObject, true)
+                //
+                //     // is the "remove after drop" checkbox checked?
+                //     if ($('#drop-remove').is(':checked')) {
+                //         // if so, remove the element from the "Draggable Events" list
+                //         $(this).remove()
+                //     }
+                //
+                // }
             });
         });
-        $("#companies").change(function(){
+        $("#companies").change(function () {
             var company = $("#companies").val();
-            if(company != 0)
-            {
+            if (company != 0) {
 
-            $.ajax({
-               type:"GET",
-               url:"{{route('getDataForCalendar')}}",
-               dataType:"json",
-               data:{'company':company},
-               success:function(data){
+                $.ajax({
+                    type: "GET",
+                    url: "{{route('getDataForCalendar')}}",
+                    dataType: "json",
+                    data: {'company': company},
+                    success: function (data) {
 
-                   $("#calendarDiv").show();
-                   // let i;
-                   // $('#calendar').fullCalendar('removeEvents',function() {
-                   //     return true;
-                   // });
-                   // for(i = 0; i < data.length; i++)
-                   // {
-                   //
-                   //     $('#calendar').fullCalendar('renderEvent', {
-                   //         title: data[i].start_time+' - '+data[i].end_time,
-                   //         start: data[i].date,
-                   //         allDay: true,
-                   //         backgroundColor: '#f56954', //red
-                   //             borderColor: '#f56954' //red
-                   //     }, 'stick');
-                   // }
+                        $("#calendarDiv").show();
+                        let i;
+                        $('#calendar').fullCalendar('removeEvents', function () {
+                            return true;
+                        });
+                        for (i = 0; i < data.length; i++) {
 
-               }
-            });
+                            $('#calendar').fullCalendar('renderEvent', {
+                                title: data[i].start_time + ' - ' + data[i].end_time,
+                                id: data[i].rel_id,
+                                start: data[i].date,
+                                allDay: true,
+                                company: data[i].company_name,
+                                backgroundColor: '#f56954', //red
+                                borderColor: '#f56954' //red
+                            }, 'stick');
+                            console.log(data);
+                        }
+                    }
+                });
             }
-            else{
+            else {
                 $("#calendarDiv").hide();
             }
         });
