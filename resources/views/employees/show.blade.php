f433306b3cde567762f767db4d48fb52a77e35c9
Nivesh Acharya <prexevin@gmail.com>
2018-11-06 17:34:38 +0545

    language added on customize column

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -14,8 +14,9 @@
                 <div class="box box-info">
                     <div class="box-header with-border">
 
-                        <h3 class="box-title">@lang('employee.EmployeesList')<a href="{{ route('employees.uploadForm') }}"
-                                                               class="btn btn-link" style="margin-left:10px">
+                        <h3 class="box-title">@lang('employee.EmployeesList')<a
+                                    href="{{ route('employees.uploadForm') }}"
+                                    class="btn btn-link" style="margin-left:10px">
                                 <small>@lang('employee.UploadNew')</small>
                             </a></h3>
                         <div class="box-tools pull-right">
@@ -25,7 +26,8 @@
                                     <i class="fa fa-wrench"></i>
                                 </button>
                                 <ul class="dropdown-menu" role="menu">
-                                    <li><a href="#" class='dropdown-item' id='view_columns'>@lang('employee.CustomizeColumns')</a></li>
+                                    <li><a href="#" class='dropdown-item'
+                                           id='view_columns'>@lang('employee.CustomizeColumns')</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -161,227 +163,227 @@
 
                                                                 <option
                                                                     <?= ($cell->{$column->field_name} == 1) ? 'selected="selected"' : ''?> value="1">
-                                                                 @lang('employee.Yes')
+                                                                    @lang('employee.Yes')
                                                                 </option>
                                                                 <option
-                                                                <?= ($cell->{$column->field_name} == 0) ? 'selected="selected"' : ''?> value="0">
-                                                            @lang('employee.No')
+                                                                    <?= ($cell->{$column->field_name} == 0) ? 'selected="selected"' : ''?> value="0">
+                                                                    @lang('employee.No')
                                                                 </option>
                                                             </select>
- @else
+                                                        @else
 
- {{ $cell->{$column->field_name} }}
- @endif
- </td>
- @endif
- @endforeach
- </tr>
- @endforeach
- @else
- <tr>
- <td colspan="6">No sheets are uploaded</td>
- </tr>
- @endif
- </tbody>
- </table>
- </div>
- </div>
- </div>
- </div>
- </div>
- </section>
- <div class="modal" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
- aria-hidden="true">
- <div class="modal-dialog modal-lg" role="document">
- <div class="modal-content">
- <div class="modal-header">
- <h5 class="modal-title" id="exampleModalLongTitle">Customize Your Columns</h5>
- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
- <span aria-hidden="true">&times;</span>
- </button>
- </div>
- <div class="modal-body">
- <form name="customize_form" class='form-horizontal' action="{{ route('customize.field') }}"
- method="POST">
- <input type="hidden" name="_method" value="POST">
- <input type="hidden" name="_token" value="{{ csrf_token() }}">
- <input type="hidden" name="modified_by" value="{{ \Session::get('user_id') }}">
- <input type="hidden" name="type" value="employee">
- @foreach($customize_columns->chunk(3) as $index=>$customize_columns_index)
- <div class="form-row">
- @foreach($customize_columns_index as $field)
- <div class="col-md-4 mb-3">
- <div class="custom-control custom-checkbox">
- <input type="checkbox" class="custom-control-input"
- id="customCheck{{$field->id}}" name='customized[]'
- value='{{$field->id.'~~'.$field->status}}' {{($field->status=='y')?'checked':''}}>
- <label class="custom-control-label"
- for="customCheck{{$field->id}}">{{trans('employee.'.$field->field_name)}}</label>
- </div>
- </div>
- @endforeach
- </div>
- @endforeach
- <div class="modal-footer">
- <button type="button" class="btn btn-secondary"
- data-dismiss="modal">Close
- </button>
- <button type="submit" class="btn btn-primary">Save changes</button>
- </div>
- </form>
- </div>
- </div>
- </div>
- </div>
- @endsection @push('scripts')
- <script src='https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'></script>
- <script src='https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js'></script>
-
-
- <script>
- $(document).ready(function () {
- $('.box-body div').css({
- 'width': window.innerWidth - 90,
- 'overflow': 'scroll',
- 'height': window.innerHeight / 1.5
- });
-
- var tds = document.querySelectorAll("td.contenteditable");
- tds.forEach(function (el, index) {
- employee.inlineEditable(el, function (response) {
- if (response.errors) {
- $('#validation').text(response.message);
- $('#validation').show();
- }
- });
- })
- });
-
- $.ajaxSetup({
- headers: {
- 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
- }
- });
-
- $('#view_columns').click(function () {
- $('#exampleModalLong').modal('show');
- });
- $(".sex_class").change(function () {
- var sex = $(this).val();
- var psi = $(this).attr('data-psi_data');
- $.ajax({
- type: "POST",
- url: "{{route('updateEmployeeGender')}}",
- data: {'sex': sex, 'psi': psi},
- dataType: "json",
- async: true,
- success: function (data) {
-
- }
- });
- });
-
- $(".status_residence").change(function () {
- var status = $(this).val();
- var psi = $(this).attr('data-psi_data');
- $.ajax({
- type: "POST",
- url: "{{route('updateEmployeeStatusResidence')}}",
- data: {'status': status, 'psi': psi},
- dataType: "json",
- async: true,
- success: function (data) {
-
- }
- });
- });
-
- $(".hourly_wage").change(function () {
- var wage = $(this).val();
- var psi = $(this).attr('data-psi_data');
- // alert(wage);
- $.ajax({
- type: "POST",
- url: "{{route('updateEmployeeHourlyWage')}}",
- data: {'wage': wage, 'psi': psi},
- dataType: "json",
- async: true,
- success: function (data) {
-
- }
- });
- });
-
- $(".operating_status").change(function () {
- var status = $(this).val();
- var psi = $(this).attr('data-psi_data');
- $.ajax({
- type: "POST",
- url: "{{route('updateEmployeeOperatingStatus')}}",
- data: {'status': status, 'psi': psi},
- dataType: "json",
- async: true,
- success: function (data) {
-
- }
- });
- });
-
- $(".status").change(function () {
- var status = $(this).val();
- var psi = $(this).attr('data-psi_data');
- $.ajax({
- type: "POST",
- url: "{{route('updateEmployeeStatus')}}",
- data: {'status': status, 'psi': psi},
- dataType: "json",
- async: true,
- success: function (data) {
-
- }
- });
- });
-
- $(".viber_install").change(function () {
- var viber = $(this).val();
- var psi = $(this).attr('data-psi_data');
- $.ajax({
- type: "POST",
- url: "{{route('updateEmployeeViberInstall')}}",
- data: {'viber': viber, 'psi': psi},
- dataType: "json",
- async: true,
- success: function (data) {
-
- }
- });
- });
-
- $(document).ready(function () {
- // Setup - add a text input to each footer cell
- // Setup - add a text input to each footer cell
+                                                            {{ $cell->{$column->field_name} }}
+                                                        @endif
+                                                    </td>
+                                                @endif
+                                            @endforeach
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td colspan="6">No sheets are uploaded</td>
+                                    </tr>
+                                @endif
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="modal" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
+         aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Customize Your Columns</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form name="customize_form" class='form-horizontal' action="{{ route('customize.field') }}"
+                          method="POST">
+                        <input type="hidden" name="_method" value="POST">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <input type="hidden" name="modified_by" value="{{ \Session::get('user_id') }}">
+                        <input type="hidden" name="type" value="employee">
+                        @foreach($customize_columns->chunk(3) as $index=>$customize_columns_index)
+                            <div class="form-row">
+                                @foreach($customize_columns_index as $field)
+                                    <div class="col-md-4 mb-3">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input"
+                                                   id="customCheck{{$field->id}}" name='customized[]'
+                                                   value='{{$field->id.'~~'.$field->status}}' {{($field->status=='y')?'checked':''}}>
+                                            <label class="custom-control-label"
+                                                   for="customCheck{{$field->id}}">{{trans('employee.'.$field->field_name)}}</label>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        @endforeach
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary"
+                                    data-dismiss="modal">Close
+                            </button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection @push('scripts')
+    <script src='https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'></script>
+    <script src='https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js'></script>
 
 
- // DataTable
- var table = $('#example').DataTable();
- $("#example_filter").css("display", "none");  // hiding global search box
+    <script>
+        $(document).ready(function () {
+            $('.box-body div').css({
+                'width': window.innerWidth - 90,
+                'overflow': 'scroll',
+                'height': window.innerHeight / 1.5
+            });
 
- $('.search-input-text').keypress(function (e) {   // for text boxes
- if (e.which == 13) {
- var i = $(this).attr('data-column');  // getting column index
- var v = $(this).val();  // getting search input value
- //              if(v!='')
- table.columns(i).search(v).draw();
- }
- });
- $('.search-input-select').on('change', function () {   // for select box
- var i = $(this).attr('data-column');
- var v = $(this).val();
- table.columns(i).search(v).draw();
- });
- });
- $('#view_columns').click(function () {
- $('#exampleModalLong').modal('show');
- });
- </script>
- @endpush
+            var tds = document.querySelectorAll("td.contenteditable");
+            tds.forEach(function (el, index) {
+                employee.inlineEditable(el, function (response) {
+                    if (response.errors) {
+                        $('#validation').text(response.message);
+                        $('#validation').show();
+                    }
+                });
+            })
+        });
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $('#view_columns').click(function () {
+            $('#exampleModalLong').modal('show');
+        });
+        $(".sex_class").change(function () {
+            var sex = $(this).val();
+            var psi = $(this).attr('data-psi_data');
+            $.ajax({
+                type: "POST",
+                url: "{{route('updateEmployeeGender')}}",
+                data: {'sex': sex, 'psi': psi},
+                dataType: "json",
+                async: true,
+                success: function (data) {
+
+                }
+            });
+        });
+
+        $(".status_residence").change(function () {
+            var status = $(this).val();
+            var psi = $(this).attr('data-psi_data');
+            $.ajax({
+                type: "POST",
+                url: "{{route('updateEmployeeStatusResidence')}}",
+                data: {'status': status, 'psi': psi},
+                dataType: "json",
+                async: true,
+                success: function (data) {
+
+                }
+            });
+        });
+
+        $(".hourly_wage").change(function () {
+            var wage = $(this).val();
+            var psi = $(this).attr('data-psi_data');
+            // alert(wage);
+            $.ajax({
+                type: "POST",
+                url: "{{route('updateEmployeeHourlyWage')}}",
+                data: {'wage': wage, 'psi': psi},
+                dataType: "json",
+                async: true,
+                success: function (data) {
+
+                }
+            });
+        });
+
+        $(".operating_status").change(function () {
+            var status = $(this).val();
+            var psi = $(this).attr('data-psi_data');
+            $.ajax({
+                type: "POST",
+                url: "{{route('updateEmployeeOperatingStatus')}}",
+                data: {'status': status, 'psi': psi},
+                dataType: "json",
+                async: true,
+                success: function (data) {
+
+                }
+            });
+        });
+
+        $(".status").change(function () {
+            var status = $(this).val();
+            var psi = $(this).attr('data-psi_data');
+            $.ajax({
+                type: "POST",
+                url: "{{route('updateEmployeeStatus')}}",
+                data: {'status': status, 'psi': psi},
+                dataType: "json",
+                async: true,
+                success: function (data) {
+
+                }
+            });
+        });
+
+        $(".viber_install").change(function () {
+            var viber = $(this).val();
+            var psi = $(this).attr('data-psi_data');
+            $.ajax({
+                type: "POST",
+                url: "{{route('updateEmployeeViberInstall')}}",
+                data: {'viber': viber, 'psi': psi},
+                dataType: "json",
+                async: true,
+                success: function (data) {
+
+                }
+            });
+        });
+
+        $(document).ready(function () {
+            // Setup - add a text input to each footer cell
+            // Setup - add a text input to each footer cell
+
+
+            // DataTable
+            var table = $('#example').DataTable();
+            $("#example_filter").css("display", "none");  // hiding global search box
+
+            $('.search-input-text').keypress(function (e) {   // for text boxes
+                if (e.which == 13) {
+                    var i = $(this).attr('data-column');  // getting column index
+                    var v = $(this).val();  // getting search input value
+                    //              if(v!='')
+                    table.columns(i).search(v).draw();
+                }
+            });
+            $('.search-input-select').on('change', function () {   // for select box
+                var i = $(this).attr('data-column');
+                var v = $(this).val();
+                table.columns(i).search(v).draw();
+            });
+        });
+        $('#view_columns').click(function () {
+            $('#exampleModalLong').modal('show');
+        });
+    </script>
+@endpush
